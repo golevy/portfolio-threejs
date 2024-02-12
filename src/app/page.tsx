c3a6fb2af6856150ca5998046eb33405db16b304
Levy <golvwei@gmail.com>
2024-02-12 23:55:18 +0800

    UI (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
+    </main>
   )
 }
