29815dda32db3e410fe4065962a12aedc7cde97b
Levy <golvwei@gmail.com>
2024-02-17 22:31:54 +0800

    Nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,23 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <Hero />
-      </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </main>
+    <>
+      <title>Levy | Portfolio</title>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </main>
+    </>
   )
 }
