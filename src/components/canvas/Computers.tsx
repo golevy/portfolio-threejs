0508f8c0e6f2cb4bacfbfb64103193caf1770433
Levy <golvwei@gmail.com>
2024-02-20 23:28:35 +0800

    Computer canvas (#9)

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,7 +1,16 @@
 import * as React from "react"
+import { Canvas } from "@react-three/fiber"
 
 const Computers = () => {
   return <div>Computers</div>
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas>
+      <React.Suspense></React.Suspense>
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
